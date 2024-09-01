85426feccb9eae038e49bec87bad5d27356f9c55
LucianoToneatti <l.toneatti@alumno.um.edu.ar>
2024-09-01 11:22:41 -0300

    Modify test_Rook to improve coverage

--- test/test_rook.py
+++ test/test_rook.py
@@ -16,26 +16,32 @@ class TestRook(unittest.TestCase):
         #Aca lo que haago es decirle antees de correr alguna posición que movimientos son los esperados
         self.board[0][0] = self.rook_black
         # Movimientos horizontales y verticales de la posición (0, 0)
-        movimientos_esperados_00 = [(1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0),
-                             (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7)]
-        self.assertEqual(self.rook_black.movimientos_basicos_de_torres(0, 0, self.board), movimientos_esperados_00)
+        movimientos_esperados_11 = [(0, 1), (2, 1), (3, 1), (4, 1), (5, 1), 
+                                    (6, 1), (7, 1), (1, 0), (1, 2), (1, 3),
+                                    (1, 4), (1, 5), (1, 6), (1, 7)]
+        self.assertEqual(self.rook_black.movimientos_basicos_de_torres(1, 1, self.board), movimientos_esperados_11)
     
     def test_movimientos_basicos_de_torres_con_obstrucciones(self):
         # Colocamos la torre en la posición (3, 3) y una pieza compañera en (3, 5)
         self.board[3][3] = self.rook_black
         self.board[3][5] = Piece("BLACK")  # Aliado en el camino
+        self.board[1][3] = Piece("BLACK")
+        self.board[3][1] = Piece("BLACK")
+        self.board[5][3] = Piece("BLACK")
 
-        movimientos_esperados_33 = [(2, 3), (1, 3), (0, 3), (4, 3), (5, 3), 
-                                    (6, 3), (7, 3), (3, 2), (3, 1), (3, 0), (3, 4)]
+
+        movimientos_esperados_33 = [(2, 3), (4, 3), (3, 2), (3, 4)]
         self.assertEqual(self.rook_black.movimientos_basicos_de_torres(3, 3, self.board), movimientos_esperados_33)
 
     def test_movimientos_basicos_de_torres_con_captura(self):
         # Colocamos la torre en la posición (3, 3) y un enemigo en (3, 5)
         self.board[3][3] = self.rook_black
         self.board[3][5] = Piece("WHITE")  # Enemigo en el camino
+        self.board[1][3] = Piece("WHITE")
+        self.board[3][1] = Piece("WHITE")
+        self.board[5][3] = Piece("WHITE")
 
-        movimientos_esperados_33 = [(2, 3), (1, 3), (0, 3), (4, 3), (5, 3), (6, 3), 
-                                    (7, 3), (3, 2), (3, 1), (3, 0), (3, 4), (3, 5)]  # Se captura en (3, 5)
+        movimientos_esperados_33 = [(2, 3), (1, 3), (4, 3), (5, 3), (3, 2), (3, 1), (3, 4), (3, 5)]  # Se captura en (3, 5)
         self.assertEqual(self.rook_black.movimientos_basicos_de_torres(3, 3, self.board), movimientos_esperados_33)
 
     def test_captura_y_colision(self):
